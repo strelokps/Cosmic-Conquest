82b5bb46aed2b2c27574e39b002b9aa7c731a194
strelokps <57904277+strelokps@users.noreply.github.com>
2023-08-22 22:26:08 +0300

    22.08.2023 22:25
    UI GetPixel (not work)

--- Assets/Code/UI/SetColorInImageInOption.cs
+++ Assets/Code/UI/SetColorInImageInOption.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+//using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
@@ -8,22 +8,132 @@ using UnityEngine.UI;
 
 public class SetColorInImageInOption : MonoBehaviour
 {
-    public UnityEvent<Color> ColorPickerEvent;
+    [SerializeField] private RawImage _ranbowChart;
+    private Camera _camera;
+    [SerializeField] private Collider _collider;
+    private Texture2D _t2d;
+    [SerializeField] private Texture2D _texture;
+    [SerializeField] private Image viewColor;
+    private Material materialView;
 
-    private RectTransform _cursor;
-    [SerializeField] private Texture2D _colorChart;
 
-    public void PickerColor(BaseEventData locData)
+    private Vector2 mousePos = new Vector2();
+    private RectTransform rect;
+    private int width = 0;
+    private int height = 0;
+
+
+
+    private void Start()
     {
-        PointerEventData pointer = locData as PointerEventData;
+        _camera = Camera.main;
+        _t2d = (Texture2D)_ranbowChart.mainTexture;
+        materialView = viewColor.material;
 
-        _cursor.position = pointer.position;
-        Color pickedColor = _colorChart.GetPixel(
-            (int)(_cursor.localPosition.x *
-                  (_colorChart.width / transform.GetChild(0).GetComponent<RectTransform>().rect.width)),
-            (int)(_cursor.localPosition.y *
-                  (_colorChart.height / transform.GetChild(0).GetComponent<RectTransform>().rect.height)));
-        Debug.Log($"Цвет: {pickedColor}");
+        var rawImage = _ranbowChart.GetComponent<RawImage>();
+        rect = rawImage.GetComponent<RectTransform>();
+
+        width = (int)rect.rect.width;
+        height = (int)rect.rect.height;
+
+        _t2d = rawImage.texture as Texture2D;
+
+        var pixelData = _t2d.GetPixels();
+        Debug.Log($"Total pixels {pixelData.Length}");
+
+        var colorIndex = new List<Color>();
+        var total = pixelData.Length;
+
+        for (int i = 0; i < total; i++)
+        {
+            var color = pixelData[i];
+            if (colorIndex.IndexOf(color) == -1)
+            {
+                colorIndex.Add(color);
+            }
+        }
+        Debug.Log($"Index color {colorIndex.Count}");
+    }
+
+    private void Update()
+    {
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(rect, Input.mousePosition, Camera.main, out mousePos);
+        mousePos.x = width - (width / 2 - mousePos.x);
+        if (mousePos.x > width || mousePos.x < 0)
+            mousePos.x = -1;
+
+        mousePos.y = Mathf.Abs((height / 2 - mousePos.y) - height);
+        if (mousePos.y > height || mousePos.y < 0)
+            mousePos.y = -1;
+
+        if (Input.GetMouseButton(0))
+        {
+            if (mousePos.x > -1 && mousePos.y > -1)
+            {
+                var color = _t2d.GetPixel((int)mousePos.x, (int)mousePos.y);
+                materialView.color = color;
+            }
+        }
+    }
+
+
+    #region полурабочий вариант выбора цвета
+/*    
+[SerializeField] private Image _ranbowChart;
+    private Texture2D _t2d;
+    private int _ranbowChartCoord_X;
+    private int _ranbowChartCoord_Y;
+
+    public Image viewColor;
+    private Material r;
+
+    private void Start()
+    {
+        _t2d = (Texture2D)_ranbowChart.mainTexture;
+
+
+        _ranbowChartCoord_X = _t2d.width;
+        _ranbowChartCoord_Y = _t2d.height;
+        r = viewColor.material;
 
     }
+
+    private void Update()
+    {// Получаем экранные координаты курсора мыши
+        Vector3 mousePosition = Input.mousePosition;
+
+        // Получаем позицию изображения в экранных координатах
+        Vector3 imagePosition = Camera.main.WorldToScreenPoint(_ranbowChart.rectTransform.position);
+
+        // Вычисляем позицию курсора относительно изображения
+        Vector3 localPosition = mousePosition - imagePosition;
+
+        // Получаем размеры прямоугольника изображения
+        Rect imageRect = _ranbowChart.rectTransform.rect;
+
+        // Ограничиваем позицию курсора в пределах прямоугольника изображения
+        float clampedX = Mathf.Clamp(localPosition.x, 0, imageRect.width);
+        float clampedY = Mathf.Clamp(localPosition.y, 0, imageRect.height);
+
+        // Приводим позицию курсора относительно изображения к координатам текстуры
+        float normalizedX = clampedX / imageRect.width;
+        float normalizedY = clampedY / imageRect.height;
+
+        // Получаем размеры текстуры
+        int textureWidth = _t2d.width;
+        int textureHeight = _t2d.height;
+
+        // Вычисляем конечные координаты пикселя на текстуре
+        int pixelX = Mathf.RoundToInt(normalizedX * textureWidth);
+        int pixelY = Mathf.RoundToInt(normalizedY * textureHeight);
+
+        // Получаем цвет пикселя на текстуре
+        Color pixelColor = _t2d.GetPixel(pixelX, pixelY);
+        r.color = pixelColor;
+        Debug.Log($"normalizedX: {normalizedX} textureCoordinateX:  {clampedX}  textureWidth:  {textureWidth}  localPosition.x: {localPosition.x} _ranbowChart.rectTransform.rect.width:  {_ranbowChart.rectTransform.rect.width} pixelX:  {pixelX}");
+    }
+*/
+#endregion
+    
 }
+
