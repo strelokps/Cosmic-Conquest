d4f87c798f478ae41b0de6be0817caac4f3dc8e0
strelokps <57904277+strelokps@users.noreply.github.com>
2023-08-23 13:36:37 +0300

    23.08.2023 13:36
    GetPixel is work!

--- Assets/Code/UI/SetColorInImageInOption.cs
+++ Assets/Code/UI/SetColorInImageInOption.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.Events;
 using UnityEngine.UI;
+using System.Drawing;
+using Color = UnityEngine.Color;
 
 public class SetColorInImageInOption : MonoBehaviour
 {
@@ -33,13 +35,11 @@ public class SetColorInImageInOption : MonoBehaviour
         var rawImage = _ranbowChart.GetComponent<RawImage>();
         rect = rawImage.GetComponent<RectTransform>();
 
-        width = (int)rect.rect.width;
+        width =  (int)rect.rect.width;
         height = (int)rect.rect.height;
 
         _t2d = rawImage.texture as Texture2D;
-
         var pixelData = _t2d.GetPixels();
-        Debug.Log($"Total pixels {pixelData.Length}");
 
         var colorIndex = new List<Color>();
         var total = pixelData.Length;
@@ -52,13 +52,13 @@ public class SetColorInImageInOption : MonoBehaviour
                 colorIndex.Add(color);
             }
         }
-        Debug.Log($"Index color {colorIndex.Count}");
+        materialView.color = _t2d.GetPixel(0, 0); ;
     }
 
     private void Update()
     {
         RectTransformUtility.ScreenPointToLocalPointInRectangle(rect, Input.mousePosition, Camera.main, out mousePos);
-        mousePos.x = width - (width / 2 - mousePos.x);
+        mousePos.x = width - (width / 2 - mousePos.x );
         if (mousePos.x > width || mousePos.x < 0)
             mousePos.x = -1;
 
@@ -70,70 +70,14 @@ public class SetColorInImageInOption : MonoBehaviour
         {
             if (mousePos.x > -1 && mousePos.y > -1)
             {
-                var color = _t2d.GetPixel((int)mousePos.x, (int)mousePos.y);
+                var color = _t2d.GetPixel((int)(mousePos.x * (_t2d.width / rect.rect.width)), (int)mousePos.y);
                 materialView.color = color;
             }
         }
     }
 
 
-    #region полурабочий вариант выбора цвета
-/*    
-[SerializeField] private Image _ranbowChart;
-    private Texture2D _t2d;
-    private int _ranbowChartCoord_X;
-    private int _ranbowChartCoord_Y;
 
-    public Image viewColor;
-    private Material r;
-
-    private void Start()
-    {
-        _t2d = (Texture2D)_ranbowChart.mainTexture;
-
-
-        _ranbowChartCoord_X = _t2d.width;
-        _ranbowChartCoord_Y = _t2d.height;
-        r = viewColor.material;
-
-    }
-
-    private void Update()
-    {// Получаем экранные координаты курсора мыши
-        Vector3 mousePosition = Input.mousePosition;
-
-        // Получаем позицию изображения в экранных координатах
-        Vector3 imagePosition = Camera.main.WorldToScreenPoint(_ranbowChart.rectTransform.position);
-
-        // Вычисляем позицию курсора относительно изображения
-        Vector3 localPosition = mousePosition - imagePosition;
-
-        // Получаем размеры прямоугольника изображения
-        Rect imageRect = _ranbowChart.rectTransform.rect;
-
-        // Ограничиваем позицию курсора в пределах прямоугольника изображения
-        float clampedX = Mathf.Clamp(localPosition.x, 0, imageRect.width);
-        float clampedY = Mathf.Clamp(localPosition.y, 0, imageRect.height);
-
-        // Приводим позицию курсора относительно изображения к координатам текстуры
-        float normalizedX = clampedX / imageRect.width;
-        float normalizedY = clampedY / imageRect.height;
-
-        // Получаем размеры текстуры
-        int textureWidth = _t2d.width;
-        int textureHeight = _t2d.height;
-
-        // Вычисляем конечные координаты пикселя на текстуре
-        int pixelX = Mathf.RoundToInt(normalizedX * textureWidth);
-        int pixelY = Mathf.RoundToInt(normalizedY * textureHeight);
-
-        // Получаем цвет пикселя на текстуре
-        Color pixelColor = _t2d.GetPixel(pixelX, pixelY);
-        r.color = pixelColor;
-        Debug.Log($"normalizedX: {normalizedX} textureCoordinateX:  {clampedX}  textureWidth:  {textureWidth}  localPosition.x: {localPosition.x} _ranbowChart.rectTransform.rect.width:  {_ranbowChart.rectTransform.rect.width} pixelX:  {pixelX}");
-    }
-*/
-#endregion
     
 }
 
