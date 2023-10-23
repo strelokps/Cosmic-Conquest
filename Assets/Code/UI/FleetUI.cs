a04b4231eaeeec61738b86475f86ef30c561f9d3
strelokps <strelokps@gmail.com>
2023-10-23 21:53:03 +0300

    23.10.2023 0.1.118.0
    add fleet UI

---
+++ Assets/Code/UI/FleetUI.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro.EditorUtilities;
+using UnityEngine;
+
+//TODO 
+// Сделать отображение кол-ва кораблей во флоте через event
+
+
+public class FleetUI : MonoBehaviour
+{
+    private Transform _selfTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _selfTransform = transform;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
