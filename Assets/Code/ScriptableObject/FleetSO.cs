0de6812aa9e13a72c383f52297ff823af3454a25
strelokps <strelokps@gmail.com>
2023-10-25 18:10:55 +0300

    24.10.2023 0.1.217.0
    add FleetSO
    add pref fleet
    fix scale model fleet

--- Assets/Code/ScriptableObject/FleetSO.cs
+++ Assets/Code/ScriptableObject/FleetSO.cs
@@ -2,8 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Fleet_SO", menuName = "CosmicCon/Config/VersionBuildSO", order = 51)]
+[CreateAssetMenu(fileName = "Fleet_SO", menuName = "CosmicCon/Config/FleetSO", order = 51)]
 public class FleetSO : ScriptableObject
 {
-    [SerializeField] private GameObject _prefab_Fleet;
+    [SerializeField] private GameObject _prefab_HumanFleet;
+    [SerializeField] private GameObject _prefab_ProtosFleet;
+    [SerializeField] private GameObject _prefab_UFOFleet;
+
+    public GameObject GetHumanFleet()
+    { 
+        return _prefab_HumanFleet;
+    }
+    public GameObject GetProtosFleet() 
+    {
+        return _prefab_ProtosFleet;
+    }
+    public GameObject GetUFOFleet()
+    {
+        return _prefab_UFOFleet;
+    }
+
 }
